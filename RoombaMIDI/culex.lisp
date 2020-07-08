ad3753f7c7c5b6d277887be84187bec385661e40
Christian <crfleisc@ucalgary.ca>
2020-07-08 09:50:15 -0600

    Added sample song CSV and LISP files

---
+++ RoombaMIDI/culex.lisp
@@ -0,0 +1,52 @@
+;0, 0, Header, 0, 1, 384
+;1, 0, Start_track
+;1, 0, Channel_prefix, 0
+;1, 0, Title_t, "iRobot Single Voice Culex"
+;1, 0, Program_c, 0, 81
+;1, 0, Sequencer_specific, 52, 5, 15, 18, 3, 3, 127, 127, 0, 255, 1, 4, 40, 83, 112, 101, 97, 107, 101, 114, 115, 32, 40, 82, 101, 97, 108, 116, 101, 107, 32, 72, 105, 103, 104, 32, 68, 101, 102, 105, 110, 105, 116, 105, 111, 110, 32, 65, 117, 100, 105, 111, 41
+;1, 0, Sequencer_specific, 15, 5, 15, 28, 50, 48, 49, 53, 46, 48, 50, 46, 48, 49, 1, 3
+;1, 0, Sequencer_specific, 44, 5, 15, 45, 77, 105, 99, 114, 111, 115, 111, 102, 116, 32, 83, 97, 110, 115, 32, 83, 101, 114, 105, 102, 44, 56, 46, 50, 53, 44, 70, 97, 108, 115, 101, 44, 70, 97, 108, 115, 101, 44, 49, 44, 48
+;1, 0, Time_signature, 4, 2, 96, 8
+;1, 0, Sequencer_specific, 3, 0, 0, 65
+;1, 0, Tempo, 352941
+(iRobot.execute-raw "140 0 16 36 8 48 8 47 8 48 8 39 8 48 8 38 8 48 8 36 8 48 8 47 8 48 8 43 2 45 2 46 2")
+(iRobot.execute-raw "141 0")
+(iRobot.execute-raw "155 16")
+(iRobot.execute-raw "140 1 16 48 2 50 2 52 2 53 2 54 2 55 2 57 2 58 2 60 2 62 2 64 2 65 2 66 2 67 16 79 16 78 32")
+(iRobot.execute-raw "141 1")
+(iRobot.execute-raw "155 15")
+(iRobot.execute-raw "140 2 16 36 8 48 8 74 8 75 8 77 16 75 8 74 8 36 8 36 8 72 32 48 8 38 8 48 8 72 16 74 16 75 16")
+(iRobot.execute-raw "141 2")
+(iRobot.execute-raw "155 29")
+(iRobot.execute-raw "140 3 16 82 16 82 32 39 8 48 8 38 8 48 8 81 32 39 8 48 8 38 8 48 8 81 32 39 8 48 8 38 8 48 8")
+(iRobot.execute-raw "141 3")
+(iRobot.execute-raw "155 33")
+(iRobot.execute-raw "140 4 16 80 40 78 8 79 8 75 8 74 8 72 8 47 8 48 8 39 8 48 8 38 8 48 8 36 8 48 8 47 8 48 8")
+(iRobot.execute-raw "141 4")
+(iRobot.execute-raw "155 25")
+(iRobot.execute-raw "140 5 16 39 8 66 8 67 8 63 8 60 8 48 8 59 8 60 8 62 8 50 8 60 8 62 8 63 8 51 8 62 8 63 8")
+(iRobot.execute-raw "141 5")
+(iRobot.execute-raw "155 20")
+(iRobot.execute-raw "140 6 16 43 2 45 2 46 2 48 2 50 2 52 2 53 2 54 2 55 2 57 2 58 2 60 2 62 2 64 2 65 2 66 2")
+(iRobot.execute-raw "141 6")
+(iRobot.execute-raw "155 5")
+(iRobot.execute-raw "140 7 16 77 16 80 16 79 16 80 16 84 8 80 8 79 8 80 8 79 16 77 16 70 8 72 8 73 32 73 8 75 8 77 24")
+(iRobot.execute-raw "141 7")
+(iRobot.execute-raw "155 34")
+(iRobot.execute-raw "140 8 16 78 8 80 8 78 8 77 8 78 8 80 16 84 32 38 8 48 8 68 16 72 32 38 8 48 8 53 16 56 32 38 8")
+(iRobot.execute-raw "141 8")
+(iRobot.execute-raw "155 35")
+(iRobot.execute-raw "140 9 16 48 8 41 16 44 32 38 8 48 8 78 16 82 16 80 16 82 16 85 8 82 8 80 8 82 8 80 16 78 16 75 16")
+(iRobot.execute-raw "141 9")
+(iRobot.execute-raw "155 34")
+(iRobot.execute-raw "140 10 16 78 16 77 16 78 16 82 8 78 8 77 8 78 8 77 16 75 16 78 8 80 8 81 80 81 16 83 16 84 96 84 16")
+(iRobot.execute-raw "141 10")
+(iRobot.execute-raw "155 55")
+(iRobot.execute-raw "140 11 16 86 16 87 96 43 2 45 2 46 2 48 2 50 2 52 2 53 2 54 2 55 2 57 2 58 2 60 2 62 2 64 2")
+(iRobot.execute-raw "141 11")
+(iRobot.execute-raw "155 22")
+(iRobot.execute-raw "140 12 16 65 2 66 2 55 2 55 8 55 8 55 2 55 8 55 8 55 2 55 8 55 8 55 2 55 8 55 8 80 2 80 8")
+(iRobot.execute-raw "141 12")
+(iRobot.execute-raw "155 14")
+;1, 52096, End_track
+;0, 0, End_of_file
